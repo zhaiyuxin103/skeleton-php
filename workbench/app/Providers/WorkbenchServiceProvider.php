edc0d38d36292fcb13c84a520399c718e115cc6b
GitHub Action <actions@github.com>
2025-09-21 02:56:52 +0000

    [rector] Rector fixes

--- workbench/app/Providers/WorkbenchServiceProvider.php
+++ workbench/app/Providers/WorkbenchServiceProvider.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Workbench\App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Override;
 
 class WorkbenchServiceProvider extends ServiceProvider
 {
     /**
      * Register services.
      */
+    #[Override]
     public function register(): void
     {
         //
